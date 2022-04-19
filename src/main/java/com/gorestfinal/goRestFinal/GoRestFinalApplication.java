5026f50e24c6edb0249cfd5958860c75483dc103
christine <chrisitne.brown@bhcc.edu>
2022-04-19 19:36:49 -0400

    Initial commit

---
+++ src/main/java/com/gorestfinal/goRestFinal/GoRestFinalApplication.java
@@ -0,0 +1,13 @@
+package com.gorestfinal.goRestFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoRestFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoRestFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gorestfinal/goRestFinal/GoRestFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gorestfinal.goRestFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoRestFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
